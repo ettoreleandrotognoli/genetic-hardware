2b0477d00869a35c4e78be486867fa63f6f5b9b8
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2015-09-17 15:58:45 -0300

    Game of Life

---
+++ src/ca/BinaryCellularAutomata3D.v
@@ -0,0 +1,60 @@
+`ifndef __BINARY_CELLULAR_AUTOMATA_3D__
+`define __BINARY_CELLULAR_AUTOMATA_3D__
+
+`define beforeLine (l-1<0?Height-1:l-1)
+`define afterLine ((l+1)%Height)
+`define beforeColumn (c-1<0?Width-1:c-1)
+`define afterColumn ((c+1)%Width)
+
+module BinaryCellularAutomata3D #(
+	parameter Width = 8,
+	parameter Height = 8
+) (
+	input clk,
+	input rst,
+	input ce,
+	input [8:0]survive,
+	input [8:0]rise,
+	input [Width*Height-1:0]set,
+	output reg [Width*Height-1:0]state
+);
+
+	wire [9:0]surviveRule = {survive,1'b0};
+	wire [9:0]rizeRule = {rize,1'b0};
+
+	genvar l,c,beforeLine,afterLine,beforeColumn,afterColumn;
+	generate
+		for (l=0;l<Height;l=l+1)
+		begin: __line__
+			for (c=0;c<Width;c=c+1)
+			begin: __column__
+				wire [3:0]sum =
+					state[`beforeLine*Width + `beforeColumn] +
+					state[`beforeLine*Width + c] +
+					state[`beforeLine*Width + `afterColumn] +
+					state[l*Width + `beforeColumn] +
+					state[l*Width + c] +
+					state[l*Width + `afterColumn] +
+					state[`afterLine*Width + `beforeColumn]+
+					state[`afterLine*Width + c] +
+					state[`afterLine*Width + `afterColumn];
+				always @(posedge clk or posedge rst)
+				begin
+					if (rst)
+					begin
+						state = set;	
+					end
+					else if (clk)
+					begin
+						state[l*Width+c] = surviveRule[sum] | surviveRule[sum];
+					end
+				end	
+			end
+		end
+	endgenerate
+
+
+endmodule
+
+
+`endif
---
+++ test/ca/BinaryCellularAutomata3DTest.v
@@ -0,0 +1,49 @@
+`include "ca/BinaryCellularAutomata3D.v"
+
+
+module BinaryCellularAutomata3DTest;
+
+	parameter Width = 8;
+	parameter Height = 8;
+
+	reg clk;
+	reg rst;
+	reg ce;
+	reg [8:0]survive;
+	reg [8:0]rise;
+	reg [Width*Height-1:0]set;
+	wire [Width*Height-1:0]state;
+
+	BinaryCellularAutomata3D
+		#(.Width(Width),.Height(Height))
+	automata
+		(clk,rst,ce,survive,rise,set,state);
+
+	integer r,i;
+	initial
+	begin
+		$dumpfile("test/ca/BinaryCellularAutomata3DTest.vcd");
+		$dumpvars(0,automata);
+		set = {$random,$random};
+		clk = 1'b0;
+		rst = 1'b1;
+		ce = 1'b0;
+		survive = 9'b000010011;
+		rise = 9'b000100100;
+		#1;
+		rst = 1'b0;
+		ce = 1'b1;
+		#1;
+		for (i=0;i<5000;i=i+1)
+		begin
+			clk = ~clk;#1;
+		end
+		clk = 1'b0;
+		#1;
+		$finish;
+	end
+
+	
+
+endmodule
+
